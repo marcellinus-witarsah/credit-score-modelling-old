2bc2b48753490924c3b1bb8901718aa23509bb93
Marcellinus Aditya Witarsah <aw.marcel@gmail.com>
2024-07-13 20:35:01 +0700

    Modularize code for credit score report and credit score scaling

--- src/credit_score/credit_score_report.py
+++ src/credit_score/credit_score_report.py
@@ -1,22 +1,35 @@
 import pandas as pd
 import numpy as np
-from src.constants import CREDIT_LEVELS_DESCRIPTIONS
 from src.constants import LEVEL_BOUNDS
+from src.constants import CREDIT_LEVELS_DESCRIPTIONS
 
 
 def interpret_credit_score(
     df: pd.DataFrame,
     target_col: str,
 ) -> pd.DataFrame:
+    """
+    Interpret the credit score into predefined levels and descriptions.
 
+    Args:
+        df (pd.DataFrame): The dataframe containing credit score data.
+        target_col (str): The column name for the credit scores in the dataframe.
+
+    Returns:
+        pd.DataFrame: The dataframe with additional columns for credit level, bounds, and descriptions.
+    """
+    # 1. Define conditions to categorize credit scores based on LEVEL_BOUNDS
     conditions = [
         (df[target_col] > LEVEL_BOUNDS[i]) & (df[target_col] <= LEVEL_BOUNDS[i + 1])
         for i in range(len(LEVEL_BOUNDS) - 1)
     ]
 
+    # 2. Prepare choices for credit levels, lower bounds, and upper bounds
     level_choices = list(CREDIT_LEVELS_DESCRIPTIONS.keys())
     lower_bound_choices = LEVEL_BOUNDS[:-1]
     upper_bound_choices = LEVEL_BOUNDS[1:]
+
+    # 3. Assign credit level, lower bound, upper bound, and description based on conditions
     df["credit_level"] = np.select(conditions, level_choices)
     df["credit_lower_bound"] = np.select(conditions, lower_bound_choices)
     df["credit_upper_bound"] = np.select(conditions, upper_bound_choices)
@@ -25,8 +38,20 @@ def interpret_credit_score(
 
 
 def generate_report(df: pd.DataFrame) -> pd.DataFrame:
-    # Count every Good Users and Bad Users in each credit levels
+    """
+    Generate a report categorizing customers into credit levels and calculating the summary statistics (cumulative percentage, proportion, etc).
+
+    Args:
+        df (pd.DataFrame): The dataframe containing customer credit scores.
+
+    Returns:
+        pd.DataFrame: The report dataframe with credit levels, customer counts, and various statistics.
+    """
+
+    # 1. Categorize into credit level based on credit score:
     report_df = interpret_credit_score(df, target_col="credit_score")
+
+    # 2. Count `Good customers` and `Bad customers` in each credit level:
     report_df = (
         report_df.groupby(
             [
@@ -51,16 +76,15 @@ def generate_report(df: pd.DataFrame) -> pd.DataFrame:
         )
         .fillna(0)
     )
-
     report_df = report_df.rename_axis(None, axis=1)
     report_df["Customers"] = report_df["Good Customers"] + report_df["Bad Customers"]
 
-    # Proportion of customers exists in each credit levels
+    # 3. Calculate proportion of customers in each credit level
     report_df["Customers Rate"] = (
         report_df["Customers"] / report_df["Customers"].sum() * 100
     )
 
-    # Proportion of `Bad Customers`` in each credit levels
+    # 4. Proportion of `Bad Customers` in each credit level
     report_df["Default Rate"] = (
         report_df["Bad Customers"] / report_df["Customers"] * 100
     )
@@ -72,13 +96,13 @@ def generate_report(df: pd.DataFrame) -> pd.DataFrame:
         ::-1
     ].cumsum()
 
-    # Calculate the cumulative percentage of `Good Customers` covered from all customers (start at level 8 down to level 1)
+    # 5. Calculate the cumulative percentage of `Good Customers` from all customers
     report_df["Good Customers Coverage"] = (
         report_df["Reverse Cumulative Good Customers"]
         / report_df["Good Customers"].sum()
     )
 
-    # Calculate the cumulative percentage of `Bad Customers` covered from all customers (start at level 8 down to level 1)
+    # 6. Calculate the cumulative percentage of `Bad Customers` from all customers
     report_df["Loss Coverage"] = (
         report_df["Reverse Cumulative Bad Customers"]
         / report_df["Reverse Cumulative Customers"]
--- src/credit_score/credit_score_scaling.py
+++ src/credit_score/credit_score_scaling.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from typing import Union
+from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from src.features.woe_transformer import WOETransformer
 
@@ -8,11 +9,19 @@ from src.features.woe_transformer import WOETransformer
 class CreditScoreScaling:
     def __init__(
         self,
-        pipeline,
+        pipeline: Pipeline,
         pdo: Union[int, float],
         odds: Union[int, float],
         scorecard_points: Union[int, float],
     ):
+        """Initialize the CreditScoreScaling with model pipeline and scorecard parameters.
+
+        Args:
+            pipeline (Pipeline): The pipeline containing the trained logistic regression model and WOE transformer.
+            pdo (Union[int, float]): Points to Double the Odds.
+            odds (Union[int, float]): The odds (Bad/Good) at the scorecard points.
+            scorecard_points (Union[int, float]): The scorecard points at the specified odds.
+        """
         self.pipeline = pipeline
         self.intercept = self.pipeline[LogisticRegression.__name__].intercept_[0]
         self.n_features = len(
@@ -23,13 +32,17 @@ class CreditScoreScaling:
         self.scorecard_points = scorecard_points
         self.factor = self.pdo / np.log(2)
         self.offset = self.scorecard_points - self.factor * np.log(self.odds)
-        self.scorecard_map_dict = {}  # for mapping values to its credit point
+        self.scorecard_map_dict = {}
 
-    # Private methods
     def __generate_mapping(self, scorecard: pd.DataFrame) -> None:
-        # Create dictionary mapping in the background
-        scorecard_features = scorecard["Characteristic"].unique()
+        """
+        Generate a mapping of scorecard features and their corresponding points.
 
+        Args:
+            scorecard (pd.DataFrame): The scorecard dataframe containing characteristics, values, and points.
+        """
+        scorecard_features = scorecard["Characteristic"].unique()
+        # 1. Create a dictionary mapping each feature to its corresponding points
         for feature in scorecard_features:
             self.scorecard_map_dict[feature] = {
                 row["Value"]: row["Points"]
@@ -39,7 +52,18 @@ class CreditScoreScaling:
             }
 
     def __get_credit_point(self, feature: str, value: Union[int, float, str]) -> float:
+        """
+        Get the credit point for a given feature and value.
+
+        Args:
+            feature (str): The feature name.
+            value (Union[int, float, str]): The feature value.
+
+        Returns:
+            float: The credit point corresponding to the feature value.
+        """
         credit_point = None
+        # 1. Check if the value is a numeric type and find the corresponding credit point
         if isinstance(value, int) or isinstance(value, float):
             for interval, woe in self.scorecard_map_dict[feature].items():
                 if isinstance(interval, pd.Interval) and value in interval:
@@ -50,43 +74,77 @@ class CreditScoreScaling:
                 else credit_point
             )
         elif isinstance(value, str):
+            # 2. If the value is a string, directly get the credit point from the mapping
             credit_point = self.scorecard_map_dict[feature][value]
         return credit_point
 
-    # Public methods
     def generate_scorecard(self) -> pd.DataFrame:
+        """
+        Generate the scorecard from the model pipeline.
+
+        Returns:
+            pd.DataFrame: The generated scorecard with characteristics, Coefficients, WOE, and points.
+        """
+        # 1. Create a summary dataframe of the model features and their coefficients
         model_summary = pd.DataFrame(
             {
                 "Characteristic": self.pipeline[
                     LogisticRegression.__name__
                 ].feature_names_in_,
-                "Estimate": self.pipeline[LogisticRegression.__name__].coef_.reshape(
+                "Coefficient": self.pipeline[LogisticRegression.__name__].coef_.reshape(
                     -1
                 ),
             }
         )
 
+        # 2. Merge the model summary with the WOE dataframe from the pipeline
         self.scorecard = pd.merge(
             left=model_summary,
             right=self.pipeline[WOETransformer.__name__].woe_df,
             how="left",
             on=["Characteristic"],
         )
+
+        # 3. Calculate the points for each feature using the scorecard formula
         self.scorecard["Points"] = (self.offset / self.n_features) - self.factor * (
-            (self.scorecard["WOE"] * self.scorecard["Estimate"])
+            (self.scorecard["WOE"] * self.scorecard["Coefficient"])
             + (self.intercept / self.n_features)
         )
+
+        # 4. Generate the mapping for scorecard features and points
         self.__generate_mapping(self.scorecard)
 
     def calculate_single_credit_score(self, data: dict) -> dict:
+        """
+        Calculate the credit score for a single instance.
+
+        Args:
+            data (dict): A dictionary of feature values for a single instance.
+
+        Returns:
+            dict: A dictionary with credit points for each feature and the total credit score.
+        """
+        # 1. Calculate credit points for each feature in the data
         details = {
             feature: self.__get_credit_point(feature, value)
             for feature, value in data.items()
         }
+
+        # 2. Sum the credit points to get the total credit score
         details["credit_score"] = sum(details.values())
         return details
 
     def calculate_credit_score(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Calculate the credit scores for a dataframe of instances.
+
+        Args:
+            X (pd.DataFrame): The dataframe containing feature values for multiple instances.
+
+        Returns:
+            pd.DataFrame: A dataframe with credit points for each feature and the total credit score for each instance.
+        """
+        # 1. Apply the calculate_single_credit_score method to each instance in the dataframe
         index = X.index
         results = X.apply(
             lambda instance: self.calculate_single_credit_score(instance.to_dict()),
